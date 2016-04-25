e0a824eaf7172d399489902ef3a74da81db98135
blakflagg <dduffy2@outlook.com>
2016-04-24 20:56:06 -0700

    Added stat info to the images in the grid

--- src/components/image-preview.jsx
+++ src/components/image-preview.jsx
@@ -13,7 +13,17 @@ module.exports = React.createClass({
     >
       {this.props.animated && this.state.hovering ? this.video(): this.image()}
       {this.props.animated && !this.state.hovering ? this.icon() : null}
+      {this.state.hovering ? this.inset() : null}
     </div>
+  },
+  inset: function(){
+    return <div className="inset">
+      Views:{this.props.views}
+      <br/>
+      Upvotes: {this.props.ups}
+    </div>
+
+
   },
   image: function(){
     var link = 'http://i.imgur.com/' + this.props.id + 'h.jpg'
